47536451a697f902429fa680d3e22d424976c347
FINE100 <106653557+FINE100@users.noreply.github.com>
2022-07-01 13:07:08 +0900

    Update Calculator.java

--- HelloWorld/src/co/edu/Calculator.java
+++ HelloWorld/src/co/edu/Calculator.java
@@ -6,11 +6,7 @@ public class Calculator {
 		int result = 10 + 20;
 		System.out.println(result);
 
-		//이력 변경 추가 2022.07.01
-		System.out.println("이력변경추가.");
-		
-		//깃에서 추가한 정보입니다.
-		
+				
 	}
 
 }
