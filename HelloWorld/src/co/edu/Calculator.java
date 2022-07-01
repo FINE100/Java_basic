22e80c65d87e92583db226d7214c3abc68ae1e8d
admin <admin@YD-06>
2022-07-01 12:56:57 +0900

    second

--- HelloWorld/src/co/edu/Calculator.java
+++ HelloWorld/src/co/edu/Calculator.java
@@ -5,7 +5,12 @@ public class Calculator {
 	public static void main(String[] args) {
 		int result = 10 + 20;
 		System.out.println(result);
-	
+
+		//이력 변경 추가 2022.07.01
+		System.out.println("이력변경추가.");
+		
 	}
 
 }
+
+
