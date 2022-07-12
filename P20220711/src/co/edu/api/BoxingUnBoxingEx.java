035d6fe693392f11b7ced95661f63edad1cb11b6
admin <admin@YD-06>
2022-07-12 17:57:56 +0900

    0712

--- P20220711/src/co/edu/api/BoxingUnBoxingEx.java
+++ P20220711/src/co/edu/api/BoxingUnBoxingEx.java
@@ -13,28 +13,28 @@ public class BoxingUnBoxingEx {
 		// 로또 번호 생성. 1~45
 		int[] lotto = new int[6];
 		for (int i = 0; i < lotto.length; i++) {
-//			int temp = (int) (Math.random() * 45) + 1;
-//			lotto[i] = temp;
-//			
-//			for(int j=0; j<i ; j++) { // 현재 값과 동일한 값이 있는지 체크.
-//				if(lotto[i] == lotto[j]) {
-//					i--;
-//					break;
-//				}
-//			}
-//			System.out.print(lotto[i] + " ");
-//				
-//
-//				
-//			} 
-//		
-//	}
-//	
-//}
-// 		for문을 이용한 다른 방법 > for(int j =0~~~~) 대신 넣으면 됨
-//		for (int num : lotto) {
-//			System.out.println(num);
-//		} // 이 프로그램은 중복값이 나올 수 있다는 단점이 있음
+			int temp = (int) (Math.random() * 45) + 1;
+			lotto[i] = temp;
+			
+			for(int j=0; j<i ; j++) { // 현재 값과 동일한 값이 있는지 체크.
+				if(lotto[i] == lotto[j]) {
+					i--;
+					break;
+				}
+			}
+			System.out.print(lotto[i] + " ");
+				
+
+				
+			} 
+		
+	
+	
+
+ 		//for문을 이용한 다른 방법 > for(int j =0~~~~) 대신 넣으면 됨
+		for (int num : lotto) {
+			System.out.println(num);
+		} // 이 프로그램은 중복값이 나올 수 있다는 단점이 있음
 
 
 
@@ -51,4 +51,4 @@ public class BoxingUnBoxingEx {
 		}
 	}
 }
-}
+
